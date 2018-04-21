e52b9b0cd7cde9c9344208d119d57d4b08749c1d
康靖波 <1070217306@qq.com>
2018-04-21 16:03:44 +0800

    解决静态资源文件重定向问题

--- src/main/java/com/resumeweb/springmvc/controller/NewResumeCtrl.java
+++ src/main/java/com/resumeweb/springmvc/controller/NewResumeCtrl.java
@@ -1,5 +1,6 @@
 package com.resumeweb.springmvc.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,23 +13,38 @@ public class NewResumeCtrl {
         return "redirect:/new/step/1";
     }
 
-    @RequestMapping("/new/step/{stepCount}")
-    public String addContent(Model model, @PathVariable("stepCount") int stepCount){
-        model.addAttribute("title","新建简历");
-        model.addAttribute("page_id","3");
-        switch (stepCount){
-            case 1:
-                return "write_base";
-            case 2:
-                return "write_edu";
-            case 3:
-                return "write_proj";
+    @RequestMapping({"/new/step/{stepCount}","/new/step/{stepCount}/**"})
+    public String addContent(Model model,
+                             HttpServletRequest request,
+                             @PathVariable("stepCount") String step){
+        try{
+            int stepCount = Integer.parseInt(step);
+            model.addAttribute("title","新建简历");
+            model.addAttribute("page_id","3");
+            switch (stepCount){
+                case 1:
+                    return "write_base";
+                case 2:
+                    return "write_edu";
+                case 3:
+                    return "write_proj";
+            }
+        }catch (NumberFormatException e){
+            return redirectRoot(request);
         }
+
         return "";
     }
 
-    @RequestMapping("/new/step/{resourcesType}/**")
-    public String getStaticResources(@PathVariable("resourcesType") String type){
-        return "redirect:/" + type + "/**";
+    /**
+     * 重定向到项目的根目录
+     * @param request
+     * @return redirect path
+     */
+    public String redirectRoot(HttpServletRequest request){
+        String fullPath = request.getRequestURI();
+        String subPath = fullPath.substring(fullPath.indexOf("step") + 5);
+
+        return "redirect:/" + subPath;
     }
 }
