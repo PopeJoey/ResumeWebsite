8bff8544e4d3068bebc05a60b681609b14d7334a
康靖波 <1070217306@qq.com>
2018-05-07 12:05:05 +0800

    delete .idea

--- src/main/java/com/resumeweb/springmvc/controller/LoginCtrl.java
+++ src/main/java/com/resumeweb/springmvc/controller/LoginCtrl.java
@@ -1,6 +1,8 @@
 package com.resumeweb.springmvc.controller;
 
 import com.resumeweb.entity.UserSession;
+import com.resumeweb.service.LogAndRegisterService;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -8,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -16,17 +19,23 @@ public class LoginCtrl {
     @RequestMapping("/login")
     public String showLogin(ModelMap model){
         model.addAttribute("title","登录");
-        UserSession userSession = new UserSession();
-        model.addAttribute("UserSession",userSession);
+        model.addAttribute("UserSession",new UserSession());
         return "login";
     }
 
     @RequestMapping(value = "/tologin",method = RequestMethod.POST)
-    public String login(@Valid UserSession userSession, BindingResult result, ModelMap model){
-        if(result.hasErrors()){
+    public String login(@Valid UserSession userSession, BindingResult result, ModelMap model, HttpServletRequest request){
+        if(result.hasErrors()) {
             return "login";
         }
-        model.addAttribute("UserSession",userSession);
-        return "redirect:/homepage";
+        LogAndRegisterService service = new LogAndRegisterService();
+        if(service.logIn(userSession.getUsername(),userSession.getPassword())){
+            model.addAttribute("UserSession",userSession);
+            request.getSession().setAttribute("currentUser",userSession);
+            return "redirect:/homepage";
+        }
+        return "login";
     }
+
+    @
 }
